f56aa985b43c62c4427654fdf604241076097011
Igor Emanuel <igor17emanuel@gmail.com>
2025-03-24 13:30:15 -0300

    Start to make a carousel for HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
 import FooterComponent from '@/components/Footer/FooterComponent.vue'
 import NavbarComponent from '@/components/Header/NavbarComponent.vue'
+import CarouselComponent from '@/components/Main/CarouselComponent.vue'
 </script>
 
 <template>
   <div class="w-full">
     <NavbarComponent />
-    <img
-      src="../assets/img/banner-black-friday.png"
-      alt="Banner Black Friday"
-      class="w-full h-1/6 object-cover"
-      style="height: 50%; width: 50%"
-    />
+    <CarouselComponent />
     <FooterComponent />
   </div>
 </template>
-
-<style scoped></style>
