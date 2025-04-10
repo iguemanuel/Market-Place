36f9379660fa41c9bace6f831dafff83c861272c
Igor Emanuel <igor17emanuel@gmail.com>
2025-04-10 16:51:36 -0300

    rename productService and add new propriety

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -135,7 +135,7 @@ const handleRegister = async () => {
           <!-- Botão de Submissão -->
           <button
             type="submit"
-            class="mt-2 w-full bg-slate-800 py-2 px-4 text-white rounded-md hover:bg-slate-700 focus:bg-slate-700"
+            class="cursor-pointer mt-2 w-full bg-slate-800 py-2 px-4 text-white rounded-md hover:bg-slate-700 focus:bg-slate-700"
           >
             Criar Conta
           </button>
