014aa53ae25a78a421cc0d0d51c692b1427e1325
Igor Emanuel <igor17emanuel@gmail.com>
2025-04-15 16:22:18 -0300

    Start making a CartView and CardCartComponent

---
+++ src/views/CartView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import CardCartComponent from '@/components/Main/CardCartComponent.vue'
+</script>
+
+<template>
+  <h1>Olá mundo</h1>
+  <CardCartComponent />
+</template>
